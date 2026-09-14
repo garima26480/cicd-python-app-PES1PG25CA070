0f74062cefdd969fa5fb60001cce3676ac1e485d
garima54pundir <garima54pundir@gmail.com>
2026-09-14 15:44:01 +0530

    Fix failing test

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import add_numbers
 
 def test_add_numbers():
-    assert add_numbers(2, 3) == 4
+    assert add_numbers(2, 3) == 5
 
 def test_add_negative():
     assert add_numbers(-1, 1) == 0
